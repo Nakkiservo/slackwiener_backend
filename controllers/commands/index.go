daf899e45ad4babf9f8bdecb288823a94b8fed8c
Teemu Poikkijoki <poidokoff@gmail.com>
2017-11-12 12:17:38 +0000

    Add test command handlers

--- controllers/commands/index.go
+++ controllers/commands/index.go
@@ -6,6 +6,7 @@ import (
   "net/http"
   "github.com/nakkiservo/slackwiener_backend/logging"
   "github.com/nakkiservo/slackwiener_backend/slack_api/commands"
+  "encoding/json"
 )
 
 
@@ -13,16 +14,65 @@ import (
 func Index(params map[string]string, w http.ResponseWriter, r *http.Request) {
   logging.Log.Debug("Action at commands controller index: ", params)
 
-  w.WriteHeader(http.StatusOK)
 
   cmd, err := commands.DecodeCommandPayload(r)
 
   if err != nil {
     logging.Log.Errorf("Unable to decode payload: %s", err.Error())
+    w.WriteHeader(http.StatusOK)
     return
   }
 
-  logging.Log.Debug("Decoded command payload: ", cmd)
+  switch(cmd.Command) {
+    case "/testcommand":
+      HandleTestCommand(cmd, w)
+    case "/testinchannel":
+      HandleTestInChannelCommand(cmd,w)
+    default:
+      logging.Log.Debugf("Token: %s, Command: %s, Text: %s", cmd.Token, cmd.Command, cmd.Text)
+  }
+}
+
+func HandleTestCommand(cmd *commands.SlackCommandPayload, w http.ResponseWriter) {
+
+  w.Header().Set("Content-Type", "application/json")
+
+  payload := make(map[string]interface{})
+
+  payload["text"] = "You have invoked the TESTCOMMAND! OH NOES!"
+  payload["attachments"] = []map[string]string{
+    {
+     "text": "Your arguments were: " + cmd.Text,
+    },
+  }
+
+  json.NewEncoder(w).Encode(&payload)
+}
+
+func HandleTestInChannelCommand(cmd *commands.SlackCommandPayload, w http.ResponseWriter) {
+
+  w.Header().Set("Content-Type", "application/json")
+
+  payload := make(map[string]interface{})
+
+  payload["text"] = cmd.UserName + " has invoked this spammy test command. We are truly sorry"
+  payload["response_type"] = "in_channel"
+
+  var payloadText string
+
+  if cmd.Text == "" {
+      payloadText = "The bastard gave no arguments either. :("
+  } else {
+      payloadText = "They thought the following arguments would do something: " + cmd.Text
+  }
+
+  payload["attachments"] = []map[string]string{
+    {
+     "text": payloadText,
+    },
+  }
+
+  json.NewEncoder(w).Encode(&payload)
 }
 
 
